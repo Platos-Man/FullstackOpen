d867d60baedc237ce18afe544a4edb851c6aa73b
Platos-Man <joonasru@tutanota.com>
2023-06-01 10:47:50 +0300

    learned some new things

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,8 +1,22 @@
-const App = () => {
-  console.log('Hello from component')
+
+const Hello = (props) => {
+  console.log(props)
   return (
     <div>
-      <p>Hello world</p>
+      <p>
+        Hello {props.name}, you are {props.age} years old {'\u{1F346}'} {'\u{1F4A6}'} {'\u{1F4A6}'}
+      </p>
+    </div>
+  )
+}
+const App = () => {
+  const name = 'PeePeePooPoo'
+  const age = 1
+
+  return (
+    <div>
+      <Hello name='Sami' age={26 + 9} />
+      <Hello name={name} age={age} />
     </div>
   )
 }
