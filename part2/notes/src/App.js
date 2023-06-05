41da629ea5d0a15a330b80e748f9b44945ccb112
Platos-Man <joonasru@tutanota.com>
2023-06-05 15:58:03 +0300

    stuff

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -1,18 +1,29 @@
+import { useState } from 'react'
 import Note from './components/Note'
 
 const App = ({ notes }) => {
+  const [allNotes, setNotes] = useState(notes);
+  const [newNote, setNewNote] = useState('note')
+
+  const addNote = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+  }
+
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        <ul>
-          {notes.map(note => 
-            <Note key={note.id} note={note} />
-          )}
-        </ul>
+        {notes.map(note =>
+          <Note key={note.id} note={note} />
+        )}
+        <form onSubmit={addNote}>
+          <input value={newNote} />
+          <button type="submit">save</button>
+        </form>
       </ul>
     </div>
   )
 }
 
-export default App
+export default App;
