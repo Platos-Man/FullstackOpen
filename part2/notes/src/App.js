3a9419becb5837c27f051e74c24a2b32885f4f14
Platos-Man <joonasru@tutanota.com>
2023-06-08 09:52:33 +0300

    notes app functionality

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -4,21 +4,44 @@ import Note from './components/Note'
 const App = ({ notes }) => {
   const [allNotes, setNotes] = useState(notes);
   const [newNote, setNewNote] = useState('note')
+  const [showAll, setShowAll] = useState(false)
 
   const addNote = (event) => {
     event.preventDefault()
-    console.log('button clicked', event.target)
+    const noteObject = {
+      id: allNotes.length + 1,
+      content: newNote,
+      important: Math.random() < 0.5,
+    }
+    setNotes(allNotes.concat(noteObject))
+    setNewNote('')
+    console.log(allNotes)
   }
+
+  const handeNoteChange = event => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll ? allNotes : allNotes.filter(note => note.important)
 
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)} >
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
       <ul>
-        {notes.map(note =>
+        {notesToShow.map(note =>
           <Note key={note.id} note={note} />
         )}
         <form onSubmit={addNote}>
-          <input value={newNote} />
+          <input
+            value={newNote}
+            onChange={handeNoteChange}
+          />
           <button type="submit">save</button>
         </form>
       </ul>
