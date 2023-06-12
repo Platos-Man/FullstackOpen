575e0fbbdbff22f818bf0abb48137193235d05fb
Platos-Man <joonasru@tutanota.com>
2023-06-12 15:41:58 +0300

    tried to implement weather api

--- part2/countries/src/services/countries.js
+++ part2/countries/src/services/countries.js
@@ -4,6 +4,7 @@ const baseUrl = "https://studies.cs.helsinki.fi/restcountries/api";
 
 const getAll = () => {
   const request = axios.get(`${baseUrl}/all`);
+  console.log(request);
   return request.then((response) => response.data);
 };
 
--- part2/countries/src/services/weather.js
+++ part2/countries/src/services/weather.js
@@ -1,1 +1,15 @@
 import axios from "axios";
+
+const baseUrl = "https://api.openweathermap.org/data/3.0/onecall?";
+
+const api_key = process.env.REACT_APP_API_KEY;
+console.log(api_key);
+
+const get = (lat, lon) => {
+  const request = axios.get(`${baseUrl}lat=${lat}&lon=${lon}&appid=${api_key}`);
+  // const request = axios.get("https://api.openweathermap.org/data/3.0/onecall?lat=38.8&lon=12.09&callback=test");
+  return request.then((response) => response.data);
+};
+
+const exportMethdods = { get };
+export default exportMethdods;
